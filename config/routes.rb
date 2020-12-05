427b3b0053a2c87b3b5f379d1616a7fb353f264b
dabadahiburi <yuuki11250310495@gmail.comm>
2020-12-05 21:03:35 +0900

    users create

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'mypage',    to:'users#me'
+  post 'login',    to: 'sessions#create'
+  delete 'logout', to:'sessinons#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :users, only: %i[create new]
+  resources :boards
+  resources :comments, only: %i[create destory]
+end
