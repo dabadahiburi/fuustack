4cf049896db4518919e4197bfcd356f6dcbfafdf
dabadahiburi <yuuki11250310495@gmail.com>
2021-05-24 13:32:24 +0900

    finished

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,8 +3,8 @@
 # Table name: comments
 #
 #  id         :integer          not null, primary key
-#  comment    :text(65535)
-#  name       :string(255)
+#  comment    :text(65535)      not null
+#  name       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :integer
